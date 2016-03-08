b43332226fb46869c85a2635b73850bd4d9b267b
NickTheTurtle <xudo61289@scis-his.org>
2016-03-08 22:23:38 +0800

    Changed admin

--- server/app.js
+++ server/app.js
@@ -1,38 +1,37 @@
 //add usernames of admins here
-var admins = [];
 //allow admin users to insert, update, and remove menus
 Menus.allow({
   insert: function (userId) {
-    return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
+    return Meteor.users.findOne({_id: userId}).admin;
   },
   update: function (userId) {
-    return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
+    return Meteor.users.findOne({_id: userId}).admin;
   },
   remove: function () {
-    return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
+    return Meteor.users.findOne({_id: userId}).admin;
   }
 });
 //allow admin users to set users to paid or not paid
 Meteor.methods({
   "setUserToPaid": function (userId) {
-    if (admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
+    if (Meteor.users.findOne({_id: this.userId}).admin) {
       Meteor.users.update({_id: userId}, {$set: {paid: true}});
     }
   },
   "setUserToNotPaid": function (userId) {
-    if (admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
+    if (Meteor.users.findOne({_id: this.userId}).admin) {
       Meteor.users.update({_id: userId}, {$set: {paid: false}});
     }
   },
   "setUserToAdmin": function (userId) {
-    if (admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
-      admins.push(Meteor.users.findOne({_id: this.userId}).username);
+    if (Meteor.users.findOne({_id: this.userId}).admin) {
+      Meteor.users.update({_id: userId}, {$set: {admin: true}});
     }
   }
 });
 Meteor.publish("menus", function () {
   //publish menus to users who have set the paid field to true or admin users
-  if (Meteor.users.findOne({_id: this.userId}).paid || admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
+  if (Meteor.users.findOne({_id: this.userId}).paid || Meteor.users.findOne({_id: this.userId}).admin) {
     return Menus.find();
   }
 });
