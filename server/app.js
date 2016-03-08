5f03312bea4a4b48d1ff508bf93164d5ec160c28
NickTheTurtle <NickTheTurtle@users.noreply.github.com>
2016-03-08 08:07:59 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,5 @@
-var admins = [];
-Meteor.users.allow({
-  update: function (userId) {
-    return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
-  }
-});
+var admins = []; //add usernames of admins here
+//allow admin users to insert, update, and remove menus
 Menus.allow({
   insert: function (userId) {
     return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
@@ -15,7 +11,21 @@ Menus.allow({
     return admins.indexOf(Meteor.users.findOne({_id: userId}).username) !== -1;
   }
 });
+//allow admin users to set users to paid or not paid
+Meteor.methods({
+  "setUserToPaid", function (userId) {
+    if (admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
+      Meteor.users.update({_id: userId}, {$set: {paid: true}});
+    }
+  },
+  "setUserToNotPaid": function (userId) {
+    if (admins.indexOf(Meteor.users.findOne({_id: this.userId}).username) !== -1) {
+      Meteor.users.update({_id: userId}, {$set: {paid: false}});
+    }
+  }
+});
 Meteor.publish("menus", function () {
+  //publish menus to users who have set the paid field to true
   if (Meteor.users.findOne({_id: this.userId}).paid) {
     return Menus.find();
   }
